fb3d90c2abae5089c0e9d21cb2a86a55782e47cc
HMoreira2020 <moreiraheather@gmail.com>
2020-10-22 13:36:45 -0700

    Remove deck_id column from questions

--- prep-deck-frontend/src/index.js
+++ prep-deck-frontend/src/index.js
@@ -1,15 +1,23 @@
-
-// const startButton = document.getElementById('start-btn')
+const userDisplayDiv = document.getElementById('user-display')
+const startButton = document.getElementById('start-btn')
+const topArea = document.querySelector('ul.top-area')
+const questionContainer = document.getElementById('question-container')
 // test that we can get data from the backend
 const BACKEND_URL = 'http:localhost:3000';
 fetch(`${BACKEND_URL}/users`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
 
+
+  fetch(`${BACKEND_URL}/questions`)
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse));
+  
+
+
 //event listener on signup form to send data to users#create and create user
 //also hides sign in form and displays main prep deck 
 const myForm = document.getElementById('myForm');
-
 myForm.addEventListener('submit', function(e) {
     e.preventDefault();
     const usersUrl = "http://localhost:3000/users"
@@ -30,12 +38,21 @@ myForm.addEventListener('submit', function(e) {
           body: JSON.stringify(formData)
         })
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+          console.log(json)
+          myForm.style.display = "none"
+          topArea.classList.add("hide")
+          startButton.classList.remove("hide")
+          let newUserP = document.createElement('para')
+          newUserP.innerText = `Hello, ${json.first_name}`
+          userDisplayDiv.append(newUserP)
+          
+        })
 
 })
 
 
-
+//defining variables for show/hide login/signup forms 
 const lis = document.querySelectorAll("li.tab")
 const tabContent = document.querySelector('.tab-content')
 //helper method to get sibling elements
@@ -54,8 +71,8 @@ let getSiblings = function(elem){
   return siblings;
 }
 
+//add event listener to each signup or login li to show the form the user clicks on and hide the other 
 lis.forEach(li => {
-  
   li.addEventListener('click', function(e) {
     e.preventDefault();
     let siblings = getSiblings(li)
@@ -73,27 +90,47 @@ lis.forEach(li => {
       } else {
         child.style.display = "block"
       }
-
     })
-  
-  // $(target).fadeIn(600);
   
   })
 })
-        // .then(function(newUserObj) {
-        //     console.log(newUserObj)
-        //     let newUserP = document.createElement('para')
-        //     newUserP.innerText = `Hello, ${newUserObj.first_name}`
-        //     myForm.append(newUserP)
-        // //  let userEmail = json.email
-        // // "skfj@gmail.com"
-        // // let userName= json.name
-        //   // let userId = json.id 
-        // // "Chris "  append these to a div to display user name 
-        // //   myForm.classList.add("hide");
-        // //   startButton.classList.remove("hide")
+
+const myLoginForm = document.getElementById('myLoginForm');
+myLoginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const loginUrl = "http://localhost:3000/login"
     
-        // })
+    let formData = {
+        email: document.getElementById("login-email").value,
+        password: document.getElementById("login-password").value
+      }
+    
+      fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify(formData)
+        })
+        .then(response => response.json())
+        .then(json => {
+          console.log(json)
+          //refactor with helper method for code below that occurs in signup and login
+          myLoginForm.style.display = "none"
+          topArea.classList.add("hide")
+          startButton.classList.remove("hide")
+          let newUserP = document.createElement('para')
+          newUserP.innerText = `Hello, ${json.first_name}`
+          userDisplayDiv.append(newUserP)
+
+        })
+
+})
+
+
+
+    
         
     
       
