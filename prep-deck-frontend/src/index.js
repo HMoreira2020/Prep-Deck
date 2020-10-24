d012f325bf17ce67094b6e4901ae2f95409aaeb7
HMoreira2020 <moreiraheather@gmail.com>
2020-10-24 15:05:59 -0700

    Cycle through fetched questions, correct/incorrect answer receives green/red border

--- prep-deck-frontend/src/index.js
+++ prep-deck-frontend/src/index.js
@@ -1,20 +1,33 @@
 const userDisplayDiv = document.getElementById('user-display')
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const topArea = document.querySelector('ul.top-area')
-const questionContainer = document.getElementById('question-container')
+
+
 const game = document.getElementById('game')
+const questionDiv = document.getElementById('question')
+const questionContent2 = document.getElementById('question-content-2')
+const choices = Array.from(document.getElementsByClassName("choice-text"))
+let currentQuestion = {} 
+let acceptingAnswers = false;
+let questionCounter = 0
+let availableQuestions = []
+let questions;
+
+
 // test that we can get data from the backend
 const BACKEND_URL = 'http:localhost:3000';
 fetch(`${BACKEND_URL}/users`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
 
-
-  fetch(`${BACKEND_URL}/questions`)
-    .then(response => response.json())
-    .then(parsedResponse => console.log(parsedResponse));
-  
-
+  //get all the questions from db
+fetch(`${BACKEND_URL}/questions`)
+.then(response => response.json())
+.then(parsedResponse => {
+  console.log(parsedResponse)
+  questions = parsedResponse
+});
 
 //event listener on signup form to send data to users#create and create user
 //also hides sign in form and displays main prep deck 
@@ -129,12 +142,68 @@ myLoginForm.addEventListener('submit', function(e) {
 
 })
 
+startGame = () => {
+  // questionCounter = 0; don't need this unless you set a MAX_QUESTIONS 
+  getNewQuestion()
+}
+
+getNewQuestion = () => {
+  if (questions.length > 0) {
+    // questionCounter ++ 
+    const questionIndex = Math.floor(Math.random() * questions.length)
+    currentQuestion = questions[questionIndex]
+    questionDiv.innerText = currentQuestion.content
+    questionContent2.innerText = currentQuestion.content_2
+
+    choices.forEach(choice => {
+      let letter = choice.dataset["letter"]
+      choice.innerText = currentQuestion["choice_" + letter]
+    })
+
+    questions.splice(questionIndex, 1)
+    acceptingAnswers = true
+  }
+}
+
+//add event listeners to each choice to compare if answer is correct and load another answer 
+choices.forEach(choice => {
+  choice.addEventListener("click", function(e) {
+    if (acceptingAnswers) {
+      acceptingAnswers = false
+      const selectedAnswer = e.target 
+      const answerPrefix = selectedAnswer.previousElementSibling.innerHTML
+
+      const answerClass = answerPrefix === currentQuestion.correct_answer ? "correct" : "incorrect"
+      //define which class to apply if user selects correct or incorrect answer 
+
+      //apply the class
+      selectedAnswer.parentElement.classList.add(answerClass)
+
+      nextButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        selectedAnswer.parentElement.classList.remove(answerClass)
+        getNewQuestion()
+         
+      })
+      
+     
+      // console.log(selectedAnswer)
+      // console.log(e.target.previousElementSibling.innerHTML)
+      // console.log(answerPrefix === currentQuestion.correct_answer)
+     
+    }
+  })
+})
+
+
 
 // on clicking on start startButton, game begins with first question displayed. rename game id later 
 startButton.addEventListener('click', function(e) {
   e.preventDefault();
   e.target.classList.add('hide')
   game.classList.remove('hide')
+  nextButton.classList.remove('hide')
+  startGame();
   // load questions into cards here 
 })
 
