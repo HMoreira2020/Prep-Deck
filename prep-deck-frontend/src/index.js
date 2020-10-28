3188f2ed31161a56bf3ada2b3a7d0588556996a6
HMoreira2020 <moreiraheather@gmail.com>
2020-10-27 18:47:44 -0700

    Give add and remove buttons an id dataset and add user id to addd button on login

--- prep-deck-frontend/src/index.js
+++ prep-deck-frontend/src/index.js
@@ -49,8 +49,11 @@ function handleUserLogin(obj, form) {
   newUserP.innerText = `Hello, ${obj.first_name}`
   // #this gives the deck_id number 4 and "Main" that belogns to the user, find out how to access this in the add button 
   userDisplayDiv.append(newUserP)
+  
   user_deck_id = obj.decks[0].id
   user_deck_name = obj.decks[0].name
+  addButton.dataset["id"] = user_deck_id
+  addButton.innerHTML = `Add to ${obj.first_name}'s Deck`
   // figure out how to give the add button the user deck id as a value so that we have the users deck when we want to add that question to their deck
 }
 
