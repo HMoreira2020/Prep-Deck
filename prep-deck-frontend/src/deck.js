abd44d4e4830ad02d0de8260a1b20866f0ff22d6
HMoreira2020 <moreiraheather@gmail.com>
2020-10-28 17:08:42 -0700

    Create deck class and fetch to update deck when add button is clicked

--- prep-deck-frontend/src/deck.js
+++ prep-deck-frontend/src/deck.js
@@ -1,2 +1,25 @@
-console.log("in the deck")
+
+class Deck {
+    debugger
+    constructor(data) {
+        this.id = data.id 
+        this.name = data.name 
+        this.questions = data.questions
+        Deck.all.push(this)
+    }
+
+    
+    render() {
+        //set questions = this.questions then use startDeck and getNewQuestion methods 
+    }
+
+    static findById(id) {
+      return this.all.find(deck => deck.id === id)
+    }
+      
+}
+
+
+
+Deck.all = [] 
 
